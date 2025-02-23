258c488af4b106af7714f9c84c3577a3cac03bbc
Vital3414 <qwer1234432148@gmail.com>
2025-02-23 12:57:35 +0200

    all

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,15 @@
-export const removeLastContact = async () => {};
+import * as fs from 'node:fs/promises';
+import { readContacts } from '../utils/readContacts.js';
+import { PATH_DB } from '../constants/contacts.js';
+
+export const removeLastContact = async () => {
+  const contacts = await readContacts();
+  if (contacts.length === 0) return console.log('Ніяких контактів не знайдено');
+
+  const removeContacts = contacts.pop();
+  await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+
+  return removeContacts;
+};
 
 removeLastContact();
